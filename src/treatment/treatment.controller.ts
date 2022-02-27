9df101603bdf422604bed56eebeccf0c5c0c167f
Achmad Sidik Fauzi <asidikfauzi@gmail.com>
2022-02-27 21:14:39 +0700

    relathion getPasienById

--- src/treatment/treatment.controller.ts
+++ src/treatment/treatment.controller.ts
@@ -1,8 +1,24 @@
-import { Controller, UseFilters } from "@nestjs/common";
+import { Controller, Get, UseFilters } from "@nestjs/common";
+import { Treatment } from "./treatment.entity";
 import { TreatmentService } from "./treatment.service";
 
 @Controller('treatment')
 export class TreatmentController
 {
     constructor(private readonly treatmentService: TreatmentService){}
+
+    @Get()
+    async findAll() {
+        return {
+            data: await this.treatmentService.findAll()
+        };
+        
+    }
+
+    @Get(':id')
+    async getPasienById(id): Promise<any>{
+        return this.treatmentService.getPasienById(id);
+    }
+
+   
 }
--- src/treatment/treatment.service.ts
+++ src/treatment/treatment.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Param } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Treatment } from "./treatment.entity";
@@ -7,4 +7,22 @@ import { Treatment } from "./treatment.entity";
 export class TreatmentService 
 {
     constructor(@InjectRepository(Treatment) private readonly treatmentRepository: Repository<Treatment>){}
+
+    findAll()
+    {
+        return this.treatmentRepository.find();
+    }
+
+    findOne(id: number)
+    {
+        return this.treatmentRepository.findOneOrFail(id);
+    }
+
+    getPasienById(@Param('id') id: string)
+    {
+        return this.treatmentRepository.findOne(id, {
+            relations: ['pasien']
+        });
+    }
+
 }
